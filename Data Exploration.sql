3f5a65a09d4dc495f1fcb0ecf252aec780f83bdd
jonguyen15 <42555024+jonguyen15@users.noreply.github.com>
2024-10-29 13:05:32 -0700

    Update Data Exploration.sql

--- Data Exploration.sql
+++ Data Exploration.sql
@@ -39,13 +39,15 @@ WHERE birthyear >= 1997 AND usertype = "Subscriber"
 SELECT birthyear
 FROM `deft-processor-434616-r6.Divvy_Trips_2019_Q1.Divvy_2019`
 WHERE birthyear >= 1997 AND usertype = "Customer"
--- Code Chunk to find how many millennials are subscribers and customers
+-- Code Chunk to find how many millennials and Gen X are subscribers and customers
 SELECT birthyear
 FROM `deft-processor-434616-r6.Divvy_Trips_2019_Q1.Divvy_2019`
-WHERE birthyear >=1980 AND birthyear <= 1996 AND usertype = "Subscriber"
+WHERE birthyear >=1965 AND birthyear <= 1996 AND usertype = "Subscriber"
 
 SELECT birthyear
 FROM `deft-processor-434616-r6.Divvy_Trips_2019_Q1.Divvy_2019`
-WHERE birthyear >=1980 AND birthyear <= 1996  AND usertype = "Customer"
+WHERE birthyear >=1965 AND birthyear <= 1996  AND usertype = "Customer"
+
+
   
   
