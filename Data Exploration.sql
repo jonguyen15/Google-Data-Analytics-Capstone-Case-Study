df5d1b76bc28504c6844428453eb7934ea8dce05
jonguyen15 <42555024+jonguyen15@users.noreply.github.com>
2024-10-29 12:56:01 -0700

    Update Data Exploration.sql

--- Data Exploration.sql
+++ Data Exploration.sql
@@ -19,5 +19,17 @@ SELECT ride_length, usertype
 FROM `deft-processor-434616-r6.Divvy_Trips_2019_Q1.Divvy_2019`
 WHERE usertype = 'Customer'  AND ride_length >= '1:30:00'
 
+-- Code Chunk to find how many Cyclystic members that are GEN Z
+SELECT COUNT(birthyear)
+FROM `deft-processor-434616-r6.Divvy_Trips_2019_Q1.Divvy_2019`
+WHERE birthyear >= 1997
+-- Code Chunk to find how many Cyclystic members that are millennials
+SELECT COUNT(birthyear)
+FROM `deft-processor-434616-r6.Divvy_Trips_2019_Q1.Divvy_2019`
+WHERE birthyear >=1980 AND birthyear <= 1996
+-- Code Chunk to find how many Cyclystic members that are GEN X
+SELECT COUNT(birthyear)
+FROM `deft-processor-434616-r6.Divvy_Trips_2019_Q1.Divvy_2019`
+WHERE birthyear >=1965 AND birthyear <= 1979
 
 
