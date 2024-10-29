8398a41d6e865820b97b197e7a0bead10e518575
jonguyen15 <42555024+jonguyen15@users.noreply.github.com>
2024-10-29 12:32:33 -0700

    Update Data Exploration.sql

--- Data Exploration.sql
+++ Data Exploration.sql
@@ -1,1 +1,12 @@
-
+-- Code Chunk to find what day subscribers uses the bike the most
+SELECT day_of_week
+FROM `deft-processor-434616-r6.Divvy_Trips_2019_Q1.Divvy_2019`
+WHERE  usertype = 'Subscriber'
+GROUP BY day_of_week
+ORDER BY COUNT(*) DESC
+-- Code Chunk to find what day customers uses the bike the most
+SELECT day_of_week
+FROM `deft-processor-434616-r6.Divvy_Trips_2019_Q1.Divvy_2019`
+WHERE  usertype = 'Customer'
+GROUP BY day_of_week
+ORDER BY COUNT(*) DESC
